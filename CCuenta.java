680db919d08f375824c4596f1d50a1031502a179
Riodev28 <riodev28@gmail.com>
2026-02-27 22:39:14 +0100

    refactor: CCuenta and Main class

--- CCuenta.java
+++ CCuenta.java
@@ -1,40 +1,155 @@
+package cuentas;
+
+/**
+ * La clase CCuenta representa una cuenta bancaria sencilla
+ * sobre la que se pueden realizar operaciones de ingreso
+ * y retirada de dinero.
+ * 
+ * @author Caue Fiorini Venancio
+ */
 public class CCuenta {
 
-
+    /** Nombre del titular de la cuenta. */
     private String nombre;
+
+    /** Número identificador de la cuenta. */
     private String cuenta;
+
+    /** Saldo actual de la cuenta. */
     private double saldo;
+
+    /** Tipo de interés aplicado a la cuenta. */
     private double tipoInterés;
 
-    public CCuenta()
-    {
+    /**
+     * Constructor por defecto.
+     */
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    /**
+     * Crea una nueva cuenta bancaria con los datos indicados.
+     *
+     * @param nom   nombre del titular
+     * @param cue   número de cuenta
+     * @param sal   saldo inicial
+     * @param tipo  tipo de interés
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+        tipoInterés = tipo;
     }
 
-    public double estado()
-    {
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     *
+     * @return saldo actual
+     */
+    public double estado() {
         return saldo;
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     * Ingresa una cantidad en la cuenta, siempre que sea positiva.
+     *
+     * @param cantidad cantidad a ingresar
+     * @throws Exception si la cantidad es negativa
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         saldo = saldo + cantidad;
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    /**
+     * Retira una cantidad de la cuenta si hay saldo suficiente.
+     *
+     * @param cantidad cantidad a retirar
+     * @throws Exception si la cantidad es menor o igual que cero,
+     *                   o si no hay saldo suficiente
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         saldo = saldo - cantidad;
+    }
+
+    /**
+     * Devuelve el nombre del titular de la cuenta.
+     *
+     * @return nombre del titular
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Establece el nombre del titular de la cuenta.
+     *
+     * @param nombre nuevo nombre del titular
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Devuelve el número de cuenta.
+     *
+     * @return número de cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * Establece el número de cuenta.
+     *
+     * @param cuenta nuevo número de cuenta
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * Devuelve el saldo actual.
+     *
+     * @return saldo actual
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * Establece el saldo de la cuenta.
+     *
+     * @param saldo nuevo saldo
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * Devuelve el tipo de interés.
+     *
+     * @return tipo de interés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * Establece el tipo de interés.
+     *
+     * @param tipoInterés nuevo tipo de interés
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
     }
 }
--- Main.java
+++ Main.java
@@ -1,23 +1,50 @@
+package cuentas;
+
+/**
+ * 
+ * @author Caue Fiorini Venancio
+ */
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuenta1;
+        float cantidad;
+
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        cantidad = 2300f;  // puedes cambiar el valor para probar
+
+        operativa_cuenta(cuenta1, cantidad);
+    }
+
+    /**
+     * Realiza la operativa principal con la cuenta:
+     * muestra saldo, retira e ingresa la cantidad indicada.
+     *
+     * @param cuenta1  objeto cuenta sobre el que operar
+     * @param cantidad cantidad a retirar e ingresar
+     */
+    public static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
 
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(cantidad);
+            System.out.println("Retirada de " + cantidad + " realizada correctamente");
         } catch (Exception e) {
-            System.out.print("Fallo al retirar");
+            System.out.println("Fallo al retirar: " + e.getMessage());
         }
+
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(cantidad);
+            System.out.println("Ingreso de " + cantidad + " realizado correctamente");
         } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
+            System.out.println("Fallo al ingresar: " + e.getMessage());
         }
+
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo final es " + saldoActual);
     }
 }
